d71f4c3f2f7f8c4dfba7abd0f4ee927932f8fc58
James Heggs <james.heggs@techreturners.com>
2020-10-23 15:13:03 +0100

    Convert to static method calls

--- src/test/java/com/techreturners/exercise001/Exercise001Test.java
+++ src/test/java/com/techreturners/exercise001/Exercise001Test.java
@@ -10,9 +10,8 @@ import org.junit.Test;
 public class Exercise001Test {
         @Test
         public void checkSingles() {
-            Exercise001 ex001 = new Exercise001();
-            assertEquals(15, ex001.singles(new int []{4,5,7,5,4,8}));
-            assertEquals(19, ex001.singles(new int []{9, 10, 19, 13, 19, 13}));
-            assertEquals(12, ex001.singles(new int []{16, 0, 11, 4, 8, 16, 0, 11}));  
-    }
+            assertEquals(15, Exercise001.singles(new int []{4,5,7,5,4,8}));
+            assertEquals(19, Exercise001.singles(new int []{9, 10, 19, 13, 19, 13}));
+            assertEquals(12, Exercise001.singles(new int []{16, 0, 11, 4, 8, 16, 0, 11}));
+        }
 }
