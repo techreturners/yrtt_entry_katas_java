33b5ab3acb011edd2c127bd5a54790464611a81d
Winndie Fan <winndie@gmail.com>
2020-10-27 22:30:02 +0000

    Update Exercise004.java

--- src/main/java/com/techreturners/exercise004/Exercise004.java
+++ src/main/java/com/techreturners/exercise004/Exercise004.java
@@ -7,6 +7,45 @@ public class Exercise004 {
 
     public String pigLatin(String str) {
         // Add your code here
-        return "";
+    	String ans="";
+    	String[] wordArray = str.split("\\s");
+    	
+    	for(int i=0; i<wordArray.length ; i++) {
+    		
+		String word="";
+    		char[] charArray = wordArray[i].toCharArray();
+    		
+    		for(int j=0; j<charArray.length ; j++) {
+    			    			
+    			if( Character.isLetter(charArray[j]) )
+    				word += charArray[j];
+    			
+    			else if( !Character.isLetter(charArray[j]) ) {
+    				
+    				ans += sheepLatin(word) + charArray[j];
+    				word = "";
+    				
+    			}
+    		}
+    		
+			ans += sheepLatin(word);
+    		    		
+    		     if (i < wordArray.length-1)
+    			     ans += " ";
+    	}
+    	    	
+        return ans;
+    }
+    
+    private String sheepLatin(String s) {
+    	
+    	     String ans="";
+    	
+		if(s.length() > 1)
+			ans += s.substring(1) + s.charAt(0) + "ay";
+		else if(s.length() == 1)
+			ans += s + "ay";
+		
+		return ans;
     }
 }
