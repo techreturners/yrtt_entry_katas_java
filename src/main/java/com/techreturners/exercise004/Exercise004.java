50324c30dea0b0206fea8b4014a0b7463c0069d3
Winndie Fan <winndie@gmail.com>
2020-10-29 13:25:43 +0000

    Update and rename exercise004.java to Exercise004.java

---
+++ src/main/java/com/techreturners/exercise004/Exercise004.java
@@ -0,0 +1,34 @@
+package com.techreturners.exercise004;
+
+public class Exercise004 {
+
+     // Move the first letter of each word to the end of it, then add "ay" to the end of the word. 
+    // Leave punctuation marks untouched.
+
+    public String pigLatin(String str) {
+        // Add your code here
+    	String ans="", word="";
+    	char[] inArray = str.toCharArray();
+    	
+    	for(int i=0; i<inArray.length ; i++) {
+    		
+    		if(Character.isLetter(inArray[i]))
+    			word += inArray[i];
+    		
+    		if((word.length()>0) &&
+    		   (!Character.isLetter(inArray[i]) || i==(inArray.length-1)) ) {
+    			
+    			word = word.substring(1) + Character.toUpperCase(word.substring(0)) + "ay";
+    			
+    			if (!Character.isLetter(inArray[i]))
+    				word += inArray[i];
+    			
+    			ans += word;
+    			word = "";
+    		}
+    	} 
+	    
+        return ans;
+	    
+    }
+}
