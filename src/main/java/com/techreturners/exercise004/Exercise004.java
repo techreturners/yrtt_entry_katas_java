ccb09f99825084e0d3330c20210dfa2b9c64b7db
vllavanya <65501898+vllavanya@users.noreply.github.com>
2020-11-23 18:13:43 +0000

    Update Exercise004.java
    Code added

--- src/main/java/com/techreturners/exercise004/Exercise004.java
+++ src/main/java/com/techreturners/exercise004/Exercise004.java
@@ -6,7 +6,19 @@ public class Exercise004 {
     // Leave punctuation marks untouched.
 
     public String pigLatin(String str) {
-        // Add your code here
-        return "";
+        StringBuffer s2 = new StringBuffer();
+        char[] input = str.toCharArray();
+        char c = ' ';
+        for (int i = 0; i < input.length; i++) {
+            if (i == 0) {
+                c = input[i];
+            } else {
+                s2.append(input[i]);
+            }
+        }
+        s2.append(c);
+        s2.append("ay");
+        return "" + s2;
+       
     }
 }
