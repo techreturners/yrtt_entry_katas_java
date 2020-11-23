2b0992313936b1f898d61a9cd50143f8560d94c3
vllavanya <65501898+vllavanya@users.noreply.github.com>
2020-11-23 17:28:54 +0000

    Update Exercise002.java
    Logic Added

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -4,9 +4,14 @@ public class Exercise002 {
 
     // The clock shows 'h' hours, 'm' minutes and 's' seconds after midnight.
     // Your task is to make the 'past' function return time past midnight converted to milliseconds.
-
+    final static int SEC_IN_MIN = 60; 	
+    final static int SEC_IN_HOUR = SEC_IN_MIN * 60;
+   
     public int past(int h, int m, int s) {
-        // Your code here!
-        return 0;
+        
+        int secFromMidnight = (h * SEC_IN_HOUR + m * SEC_IN_MIN + s) * 1000;
+        return secFromMidnight;
+        
+        
     }
 }
